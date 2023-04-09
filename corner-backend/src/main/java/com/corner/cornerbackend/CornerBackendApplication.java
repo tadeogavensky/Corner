425136376c8ba7b5df9c04ab4cbcf053dfab32df
Gavensky <Tadeo.Gavensky@nestle.com>
2023-04-09 15:45:27 -0300

    push backend

---
+++ corner-backend/src/main/java/com/corner/cornerbackend/CornerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.corner.cornerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerBackendApplication.class, args);
+	}
+
+}
